54294c31c78d3cda5487a008c80d8810c7877b1c
Joel Alcala <joel@joelalcala.net>
2024-10-09 22:18:51 -0400

    Remove Add page

--- src/app/audit/AuditClient.tsx
+++ src/app/audit/AuditClient.tsx
@@ -355,10 +355,6 @@ export default function AuditClient({ initialRunId }: AuditClientProps) {
                     <File className="h-3.5 w-3.5" />
                     <span className="sr-only sm:not-sr-only">Export</span>
                   </Button>
-                  <Button size="sm" className="h-8 gap-1">
-                    <PlusCircle className="h-3.5 w-3.5" />
-                    <span className="sr-only sm:not-sr-only">Add Page</span>
-                  </Button>
                 </div>
               </div>
               {pageTypes.map((type) => (
